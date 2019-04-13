29eab37854390291df5f5856636621aa5e3aa63c
Dario J. Grinberg <dariogrinberg@gmail.com>
2019-04-13 12:04:58 -0300

    Switch color rectangle example

---
+++ ui-example/ui-example.gd
@@ -0,0 +1,24 @@
+extends Control
+
+var is_pressed = false
+
+func _draw():
+	
+	var r = Rect2(Vector2(), rect_size)
+	
+	if (is_pressed):
+		draw_rect(r, Color(1, 0, 0))
+	else:
+		draw_rect(r, Color(0, 0, 1))
+		
+func _input(event):
+	if(Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		is_pressed = !is_pressed
+		update()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
