35389828d8108808a8fb391f0d8fc8c342bf0173
Dario J. Grinberg <dariogrinberg@gmail.com>
2019-04-13 15:27:23 -0300

    screen with basic animation

--- ui-example/ui-example.gd
+++ ui-example/ui-example.gd
@@ -12,7 +12,7 @@ func _draw():
 		draw_rect(r, Color(0, 0, 1))
 		
 func _input(event):
-	if(Input.is_mouse_button_pressed(BUTTON_LEFT)):
+	if(event.is_pressed() and Input.is_mouse_button_pressed(BUTTON_LEFT)):
 		is_pressed = !is_pressed
 		update()
 # Called when the node enters the scene tree for the first time.
