0fb7fc937c33e0758009944d3dd666035e82370d
dariojg <dariogrinberg@gmail.com>
2021-03-28 19:07:27 -0300

    coin dash first part

---
+++ coin_dash/Main.gd
@@ -0,0 +1,79 @@
+extends Node
+
+export (PackedScene) var Coin
+export (int) var playtime
+
+var level
+var score
+var time_left
+var screensize
+var playing = false
+
+
+func _ready():
+	randomize()
+	screensize = get_viewport().get_visible_rect().size
+	$Player.screensize = screensize
+	$Player.hide()
+
+
+func _process(delta):
+	if playing and $CoinContainer.get_child_count() == 0:
+		level += 1
+		time_left += 5
+		spawn_coins()
+		
+			
+func new_game():
+	playing = true
+	level = 1
+	score = 0
+	time_left = playtime
+	$Player.start($PlayerStart.position)
+	$Player.show()
+	$GameTimer.start()
+	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
+
+
+func spawn_coins():
+	for i in range(4 + level):
+		var c = Coin.instance()
+		$CoinContainer.add_child(c)
+		#c.screensize = screensize  # no entiendo por c.screensize
+		c.position = Vector2(rand_range(0, screensize.x), rand_range(0, screensize.y))
+
+
+func _on_GameTimer_timeout():
+	time_left -= 1
+	$HUD.update_timer(time_left)
+	if time_left <= 0:
+		game_over()
+
+
+func _on_Player_pickup():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_Player_hurt():
+	game_over()
+	
+	
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	
+	for coin in $CoinContainer.get_children():
+		coin.queue_free()
+	
+	$HUD.show_game_over()
+	$Player.die()
+	
+
+
+
+
+func _on_HUD_start_game():
+	pass # Replace with function body.
---
+++ coin_dash/Player.gd
@@ -0,0 +1,61 @@
+extends Area2D
+
+signal pickup
+signal hurt
+
+export (int) var speed
+var velocity  = Vector2()
+var screensize = Vector2(480, 720)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	get_input()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	
+	if velocity.length() > 0:
+		$AnimatedSprite.animation = "run"
+		$AnimatedSprite.flip_h = velocity.x < 0
+	else:
+		$AnimatedSprite.animation = "idle"
+
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+
+func start(pos):
+	set_process(true)
+	position = pos
+	$AnimatedSprite.animation = "idle"
+	
+	
+func _on_Player_area_entered(area):
+	if area.is_in_group("coins"):
+		area.pickup()
+		emit_signal("pickup")
+	if area.is_in_group("obstacles"):
+		emit_signal("hurt")
+		die()
+
+
+func die():
+	$AnimatedSprite.animation = "hurt"
+	set_process(false)
